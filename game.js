76aeed8b7f540a539f4c27c79bf835f9a1f6c58d
Petr Gašparík <petr.gasparik@ami.cz>
2019-05-07 16:16:12 +0200

    update, game is broken now

--- game.js
+++ game.js
@@ -1,44 +1,72 @@
-var Game = (function() {
+"use strict";
+
+var Game = (function () {
 
     var instance = {
+        TOTHEFUTURE: 1,
+        TOTHEPAST: -1,
         lastUpdateTime: 0,
-        intervals: {},
+        future: {},
+        past: {},
+        arrowOfTime: 1,
+        gameTick: 0
     };
 
-    instance.update_frame = function(time) {
+    instance.update_frame = function (time) {
+        var delta = time - Game.lastUpdateTime;
         Game.lastUpdateTime = time;
+        Game.gameTick += Game.arrowOfTime * delta;
+        if (Game.gameTick < 0) Game.gameTick = 0;
         Game.update();
 
         // This ensures that we wait for the browser to "catch up" to drawing and other events
         window.requestAnimationFrame(Game.update_frame);
     };
 
-    instance.update = function() {
-        document.getElementById('tick').innerHTML = "Time: " + this.lastUpdateTime;
+    instance.update = function () {
+        document.getElementById('tick').innerHTML = "Time: " + Math.round(this.gameTick) + (Game.arrowOfTime == 1 ? " >>>" : " <<<");
 
-        document.getElementById('whats-next').innerHTML = "Next actions:";
+        // Future events
+        document.getElementById('future').innerHTML = "Future actions:";
+        for (var name in this.future) {
+            var data = this.future[name];
+            document.getElementById('future').innerHTML += "<br>" + data.d + ": " + name;
+            if (this.gameTick > data.d && Game.arrowOfTime == Game.TOTHEFUTURE) {
+                console.debug(this.gameTick + ": Time to " + name + "!")
+                data.c(name, this, data.d);
+            }
+        }
 
-        for (var name in this.intervals) {
-            var data = this.intervals[name];
-            document.getElementById('whats-next').innerHTML += "<br>" + data.d + ": " + name;
-            if (this.lastUpdateTime > data.d) {
-                console.debug(this.lastUpdateTime + ": Time for " + name + "!")
-                document.getElementById('history').innerHTML += "<br>" + data.d + ": " + name;
+        // Past events
+        document.getElementById('past').innerHTML = "Past actions:";
+        for (var name in this.past) {
+            var data = this.past[name];
+            document.getElementById('past').innerHTML += "<br>" + data.d + ": " + name;
+            if (this.gameTick < data.d && Game.arrowOfTime == Game.TOTHEPAST) {
+                console.debug(this.gameTick + ": Time to " + name + "!")
                 data.c(name, this, data.d);
             }
         }
     };
 
-    instance.createInterval = function(name, callback, delay) {
-        this.intervals[name] = { c: callback, d: delay, e: 0 }
+    instance.scheduleEvent = function (name, callback, gameTime) {
+        if (gameTime > 0) {
+            if (this.arrowOfTime == 1) {
+                if (this.future.) this.future = this.future.filter(name);
+                this.past[name] = { c: callback, d: gameTime, e: 0 };
+            } else if (this.arrowOfTime == -1) {
+                this.past = this.past.filter(function (e) { return e[name] !== name; });
+                this.future[name] = { c: callback, d: gameTime, e: 0 };
+            }
+        }
 
+    }
+
+    instance.changeArrowOfTime = function () {
+        Game.arrowOfTime *= -1;
     };
 
-    instance.deleteInterval = function(name) {
-        delete this.intervals[name];
-    };
-
-    instance.start = function() {
+    instance.start = function () {
         console.debug("Loading Game");
         //this.createInterval("Loading Animation", this.loadAnimation, 10);
         //this.createInterval("Loading", this.loadDelay, 1000);
@@ -49,8 +77,8 @@ var Game = (function() {
     return instance;
 }());
 
-window.onload = function() {
-    Game.createInterval("Do something", Test.something, 5000);
-    Game.createInterval("Do something else", Test.somethingElse, 6500);
+window.onload = function () {
+    Game.scheduleEvent("Do something", Test.something, 1000);
+    Game.scheduleEvent("Do something else", Test.somethingElse, 500);
     Game.start();
 };
--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-var Test = (function() {
+"use strict";
+
+var Test = (function () {
 
     var instance = {};
 
-    instance.something = function(name, self, time) {
-        Game.deleteInterval(name);
-        Game.createInterval(name, Test.something, time + 2000);
-        return;
+    instance.something = function (name, self, time) {
+        Game.scheduleEvent(name, Test.something, time + Game.arrowOfTime * 2000);
+         return;
     }
 
-    instance.somethingElse = function(name, self, time) {
-        Game.deleteInterval(name);
-        Game.createInterval(name, Test.somethingElse, time + 1500);
+    instance.somethingElse = function (name, self, time) {
+        Game.scheduleEvent(name, Test.somethingElse, time + Game.arrowOfTime * 1500);
         return;
     }
 
