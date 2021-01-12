54d2c481bdac747a552308143513a564ff88f228
Petr Gašparík <petr.gasparik@ami.cz>
2021-01-12 23:03:47 +0100

    new version

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ var game = new Phaser.Game(config);
 
 //my variables
 var timer = new Map();
+var multipliers = new Map();
+
+//UI variables
+fontSizeLabels = 20;
+labelDistance = fontSizeLabels / 16;
+cornerX = 10;
+cornerY = 10;
 
 function preload() {
     this.load.image('buttonPause', 'assets/pause_icon_0.png');
@@ -22,15 +29,22 @@ function preload() {
 
 function create() {
     //counters
-    peopleNow = 0;
-    rolesNow = 0;
-    systemsNow = 0;
+    peopleNow = 1;
+    rolesNow = 1;
+    entitlementsNow = 1;
+    systemsNow = 1;
+
+    //multipliers
+    multipliers.set('people', 1);
+    multipliers.set('roles', 1);
+    multipliers.set('entitlements', 0.5);
+    multipliers.set('systems', 1);
 
     //timers for counters
     timer.set('time', this.time.addEvent({
         delay: 100,                // ms
         callback: updateTime,
-        //args: [],
+        //args: [-],
         callbackScope: this,
         loop: true
     }));
@@ -48,8 +62,15 @@ function create() {
         callbackScope: this,
         loop: true
     }));
+    timer.set('entitlements', this.time.addEvent({
+        delay: 1000,                // ms
+        callback: recomputeEntitlements,
+        //args: [],
+        callbackScope: this,
+        loop: true
+    }));
     timer.set('systems', this.time.addEvent({
-        delay: 10000,                // ms
+        delay: 30000,                // ms
         callback: recomputeSystems,
         //args: [],
         callbackScope: this,
@@ -57,22 +78,23 @@ function create() {
     }));
 
     //labels with counters
-    timeLabel = this.add.text(16, 16, 'time: ', { fontSize: '32px', fill: '#000' });
-    peopleLabel = this.add.text(16, 50, '', { fontSize: '32px', fill: '#000' });
-    rolesLabel = this.add.text(16, 84, '', { fontSize: '32px', fill: '#000' });
-    systemsLabel = this.add.text(16, 120, '', { fontSize: '32px', fill: '#000' });
+    timeLabel = addLabel(this, 1);
+    systemsLabel = addLabel(this, 2);
+    rolesLabel = addLabel(this, 3);
+    peopleLabel = addLabel(this, 4);
+    entitlementsLabel = addLabel(this, 5);
+
+    //game state
+    paused = false;
+    gameLabel = addLabel(this, 7);
 
     //buttons
-    const buttonStop = this.add.text(16, 200, 'S', { fontSize: '32px', fill: '#000' });;
-    const buttonPlay = this.add.text(48, 200, 'P', { fontSize: '32px', fill: '#000' });;
+    const buttonStop = addLabel(this, 8, 'STOP');
+    const buttonPlay = addLabel(this, 8, 'PLAY', 3);
     buttonStop.setInteractive();
     buttonPlay.setInteractive();
     buttonStop.on('pointerup', pauseResumeGame, this);
     buttonPlay.on('pointerup', pauseResumeGame, this);
-
-    //game state
-    paused = false;
-    gameLabel = this.add.text(16, 234, '', { fontSize: '32px', fill: '#000' });
 
     updateScene();
     console.log("created");
@@ -85,40 +107,58 @@ function update() {
 }
 
 function updateTime() {
-    timeLabel.setText("time: " + Math.round(this.time.now / 100) / 10);
+    timeLabel.setText("days: " + Math.round(this.time.now / 1000));
 }
 
 function updateScene() {
-    peopleLabel.setText("people: " + peopleNow + ", delay: " + Math.round(timer.get('people').delay));
-    rolesLabel.setText("roles: " + rolesNow + ", delay: " + Math.round(timer.get('roles').delay));
-    systemsLabel.setText("systems: " + systemsNow + ", delay: " + Math.round(timer.get('systems').delay));
+    peopleLabel.setText("people: " + peopleNow + ", delay: " + Math.round(timer.get('people').delay) + ", multiplier: " + Math.round(multipliers.get('people')));
+    rolesLabel.setText("roles: " + rolesNow + ", delay: " + Math.round(timer.get('roles').delay) + ", multiplier: " + Math.round(multipliers.get('roles')));
+    entitlementsLabel.setText("entitlements: " + entitlementsNow + ", delay: " + Math.round(timer.get('entitlements').delay) + ", multiplier: " + Math.round(multipliers.get('entitlements')));
+    systemsLabel.setText("systems: " + systemsNow + ", delay: " + Math.round(timer.get('systems').delay) + ", multiplier: " + Math.round(multipliers.get('systems')));
     gameLabel.setText("status: " + (paused ? "paused" : "running"));
 }
 
 function recomputePeople() {
-    peopleNow++;
+    peopleNow = Math.round(peopleNow + multipliers.get('people'));
     timer.get('people').delay /= 1.01;
     //50ms is a limit for us
-    if (timer.get('people').delay < 50)
+    if (timer.get('people').delay < 50) {
         timer.get('people').delay = 50;
+        multipliers.set('people', multipliers.get('people') * 1.01);
+    }
     updateScene();
 }
 
 function recomputeRoles() {
-    rolesNow++;
-    timer.get('roles').delay /= 1.15;
+    rolesNow = Math.round(rolesNow + multipliers.get('roles'));
+    timer.get('roles').delay /= 1.01;
     //50ms is a limit for us
-    if (timer.get('roles').delay < 50)
+    if (timer.get('roles').delay < 50) {
         timer.get('roles').delay = 50;
+        multipliers.set('roles', multipliers.get('roles') * 1.01);
+    }
+    updateScene();
+}
+
+function recomputeEntitlements() {
+    entitlementsNow = Math.round(peopleNow * rolesNow * multipliers.get('entitlements'));
+    timer.get('entitlements').delay /= 1.01;
+    //50ms is a limit for us
+    if (timer.get('entitlements').delay < 50) {
+        timer.get('entitlements').delay = 50;
+        multipliers.set('entitlements', multipliers.get('entitlements') * 1.001);
+    }
     updateScene();
 }
 
 function recomputeSystems() {
-    systemsNow++;
+    systemsNow = Math.round(systemsNow + multipliers.get('systems'));
     timer.get('systems').delay /= 1.01;
     //50ms is a limit for us
-    if (timer.get('systems').delay < 50)
+    if (timer.get('systems').delay < 50) {
         timer.get('systems').delay = 50;
+        multipliers.set('systems', multipliers.get('systems') * 1.01);
+    }
     updateScene();
 }
 
@@ -129,6 +169,10 @@ function pauseResumeGame() {
     timer.forEach((value, key) => {
         key != 'time' ? value.paused = paused : null;
     });
-    
+
     updateScene();
+}
+
+function addLabel(thisGame, positionY, text = '', positionX = 0) {
+    return thisGame.add.text(cornerX + positionX * (fontSizeLabels + labelDistance), cornerY + (positionY - 1) * (fontSizeLabels + labelDistance), text, { fontSize: fontSizeLabels + 'px', fill: '#000' });
 }
